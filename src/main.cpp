d74ea82a5f33422d4401348e85efdbf58e681c37
pe3iva <dawgsofast@protonmail.com>
2025-03-11 18:49:03 +0100

    add item layout and check for invalid numbers

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,29 @@ int main(){
 
     std::cout << "Hello! Pick an item that you want: \n\n";
     std::cout << "List out the prices for each item using the list command\n\n";
+
     int itemChoice {};
-    std::cin >> itemChoice;
 
-    
+    std::cout << "1. Cola 2. Pepsi 3. Chips\n";
+    std::cout << "4. Chicken 5. French fries 6. Ice cream\n";
+    std::cout << "7. Rice 8. Strawberry 9. Jam\n";
+    std::cout << "           10. Pineapple\n";
 
-    do{
+
+    while(itemChoice <= 0 || itemChoice > 10){
+
         std::cout << "1. Cola 2. Pepsi 3. Chips\n";
-    }while(itemChoice <= 0 && itemChoice > 10);
+        std::cout << "4. Chicken 5. French fries 6. Ice cream\n";
+        std::cout << "7. Rice 8. Strawberry 9. Jam\n";
+        std::cout << "           10. Pineapple\n";
+
+        std::cin >> itemChoice;
+
+        if(itemChoice <= 0 || itemChoice > 10){
+            std::cout << "Wrong number entered. Must be 1 to 12.\n";
+        }
+    }
+
+
 
 }
